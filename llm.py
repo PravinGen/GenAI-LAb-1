2314a3748b354215068eadd4ba8a61e499930751
PravinGen <pmishra2120@gmail.com>
2026-04-22 16:59:21 +0530

    Add files via upload

--- llm.py
+++ llm.py
@@ -1,9 +1,14 @@
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 
-def get_llm():
+def get_llm(model: str):
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+    if not api_key:
+        raise ValueError("GOOGLE_API_KEY not found")
+
     return ChatGoogleGenerativeAI(
-        model="gemini-pro",
-        google_api_key=os.getenv("GOOGLE_API_KEY"),
-        temperature=0.7
+        model="gemini-2.5-flash",   # ✅ FINAL WORKING MODEL
+        temperature=0,
+        google_api_key=api_key
     )
